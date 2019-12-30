4469d1113e7cab98bad265b0cd7057b8d5fafa53
lulu1417 <43202244+lulu1417@users.noreply.github.com>
2019-12-30 15:33:11 +0800

    Update Connection.php

--- core/database/Connection.php
+++ core/database/Connection.php
@@ -5,7 +5,11 @@ class Connection {
 	public static function make($config) {
 
 		try {
-			return new PDO($config['connection'] . ';dbname=' . $config['name'], $config['username'], $config['password'], $config['option']);
+			return new PDO(
+				$config['connection'] . ';dbname=' . $config['name'], 
+				$config['username'], 
+				$config['password'], 
+				$config['option']);
 		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
